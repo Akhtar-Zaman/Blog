2290c89f9657fc72d7a124783bd45cad8e882dd9
Akhtar Zaman Khan <47081407+Akhtar-Zaman@users.noreply.github.com>
2020-07-07 23:54:06 +0600

    Update settings.py

--- Our_Blog/Our_Blog/settings.py
+++ Our_Blog/Our_Blog/settings.py
@@ -124,5 +124,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER ='akhtar1611319@gmail.com'
-EMAIL_HOST_PASSWORD ='akhtar12341234'
+EMAIL_HOST_USER ='@gmail.com'
+EMAIL_HOST_PASSWORD ='1234'
